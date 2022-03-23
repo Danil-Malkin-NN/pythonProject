523ff0f0b25ac418715748a7069d92a483cf2126
Danil-Malkin-NN <danilmalkin2015@gmail.com>
2022-03-23 22:58:00 +0300

    last try. It is very hard

--- leson2/main.py
+++ leson2/main.py
@@ -1,5 +1,6 @@
 import collections
 
+import numpy
 import pandas.core.groupby
 
 from leson2 import student
@@ -12,6 +13,7 @@ list_s.append(Student("Avan", 'Ivanov', '2001', 'PINZH-20', '2', [6, 4, 8, 10, 5
 list_s.append(Student('Sergey', "Sergeevich", '2001', 'PINZH-20', '2', [8, 8, 6, 6, 8]))
 list_s.append(Student('Ivan', 'Ivanov', '2002', 'PINZH-19', '3', [8, 3, 7, 6, 4]))
 
+
 # studentList.sort(key= student: student.first_name, reverse=True)
 
 def sorting(studentList):
@@ -20,33 +22,31 @@ def sorting(studentList):
     for studetn in studentList:
         print(studetn)
 
+
 def group_list(lst):
     map_student = dict.fromkeys([stud.group_name for stud in lst])
 
     for student in lst:
         key = student.group_name
-        if( map_student[key] == None):
+        if (map_student[key] == None):
             map_student[key] = [student]
         else:
             map_student[key].append(student)
 
+    for s, value in map_student.items():
 
-    for s in map_student:
         print(s)
+        marks = dict
 
-
-
-
-
+        for student in value:
+            marks.update({student.group_name + student.first_name + g: numpy.mean(student.marks_list)})
 
 
     print(map_student)
+
 
 sorting(list_s)
 
 print()
 
 group_list(list_s)
-
-
-
