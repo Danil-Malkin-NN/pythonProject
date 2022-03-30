95d9fda0cfaa369bea4e033de5f58e08bdc992cd
Danil-Malkin-NN <danilmalkin2015@gmail.com>
2022-03-30 20:50:14 +0300

    update

--- leson2/main.py
+++ leson2/main.py
@@ -56,15 +56,18 @@ print(min(list_s, key=lambda stud: int(stud.year_of_birthday)))
 
 print()
 print("Средний бал для каждого предмета каждой группы")
+
+
 def search_average_score_by_matrix(matrix):
     res = [round(average(x), 2) for x in zip(*matrix)]
     print(res)
 
 
 def search_gpa_by_students_group(groups_of_students):
-  for s, value in groups_of_students.items():
-      matrix_of_grades = [stud.marks_list for stud in value]
-      print('\nGPA for ' + s + ' group')
-      search_average_score_by_matrix(matrix_of_grades)
+    for s, value in groups_of_students.items():
+        matrix_of_grades = [stud.marks_list for stud in value]
+        print('\nGPA for ' + s + ' group')
+        search_average_score_by_matrix(matrix_of_grades)
+
 
 search_gpa_by_students_group(map_s)
